89a0b1fbc759ebb287d82e807de5036bc08f8de0
DmitriiCherkasov <100002195+DmitriiCherkasov@users.noreply.github.com>
2022-02-22 13:02:07 +1100

    Add files via upload

---
+++ sniffer_refactor.py
@@ -0,0 +1,47 @@
+import serial
+listener = serial.Serial(port='COM3', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                             bytesize=serial.EIGHTBITS, timeout=None)
+request_start_list = ['01', '02', '03', '04']
+request_start_list_two = ['0103', '0203', '0303', '0403']
+#request_start_list_three = ['0103012c', '0203012c', '0303012c', '0403012c']
+request_start_list_four = ['0103008c', '0203008c', '0303008c', '0403008c']
+response = ''
+request = ''
+request_start = ''
+generator_parameters = {'Generator number': None, 'Engine rpm' : None, 'Generator Front Bearing Temperature' : None,
+                        'Generator Rear Bearing Temperature': None, 'Engine Oil Temperature' : None, 'Battery Voltage' : None, 'Engine Operating Hours' : None, 'Engine Oil Pressure' : None,
+                        'Generator Phase A Winding Temperature' : None, 'Generator Phase B Winding Temperature' : None, 'Generator Phase C Winding Temperature' : None}
+while True:
+    request = listener.read().hex()
+    if request in ['01', '02', '03', '04']:
+        request += listener.read().hex()
+        if request in ['0103', '0203', '0303', '0403']:
+            request += listener.read(2).hex()
+            if request in ['0103008c', '0203008c', '0303008c', '0403008c']:
+                request = request + listener.read(4).hex()
+                response = listener.read(245).hex()
+#               print(response, sep='\n')
+                generator_parameters['Generator number'] = response[:2]
+#               generator_parameters['Function'] = response[2:4]
+#               generator_parameters['Byte count'] = response[4:6]
+#               generator_parameters['Generator Total Reactive Power'] = (int(response[6:14], 16) - 2000000000)/1000  # 0x008C 2 registers
+#               generator_parameters['Generator Total Percent Reactive Power'] = int(response[14:18], 16) * 0.0078125 - 251 # 1 register
+#               generator_parameters['Generator Total Real Energy Exported'] = int(response[18:26], 16) # 2 registers
+#               generator_parameters['Generator Total Reactive Energy Exported'] = int(response[26:34], 16) # 2 registers
+                #generator_parameters['Generator Average Line-Neutral AC RMS Voltage'] = int(response[34:38], 16)  # 1 register
+                generator_parameters['Generator Front Bearing Temperature'] = int(response[38:42], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Generator Rear Bearing Temperature'] = int(response[42:46], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Generator Phase A Winding Temperature'] = int(response[46:50], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Generator Phase B Winding Temperature'] = int(response[50:54], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Generator Phase C Winding Temperature'] = int(response[54:58], 16) * 0.03125 - 273 #  1 register
+                generator_parameters['Engine Oil Temperature'] = int(response[238:242], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Engine Oil Pressure'] = int(response[242:246], 16) * 0.125 # 1 register
+                generator_parameters['Engine Coolant Temperature'] = int(response[246:250], 16) * 0.03125 - 273 # 1 register
+                generator_parameters['Battery Voltage'] = round(int(response[250:254], 16) * 0.05, 1) # 1 register
+                generator_parameters['Engine rpm'] =  int(int(response[254:258], 16) * 0.125) # 1 register
+                generator_parameters['Engine Operating Hours'] = int(int(response[258:266], 16) * 0.05) # 2 registers
+                #generator_parameters['Automatic Start/Stop State'] = int(response[266:270], 16) # 1 register
+                with open('data.txt', 'a') as data:
+                    data.write(str(generator_parameters))
+                    data.write('\n')
+                print(generator_parameters)
